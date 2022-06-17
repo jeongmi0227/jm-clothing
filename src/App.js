d9064a4717bf7987bdf0c8f93b78fb3321b16b87
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-06-17 03:17:38 -0400

    Add React-router-dom into Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Home from './components/routes/home.component';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from './components/routes/navigation/navigation.component';
+
+const Shop = () => {
+  return <h1>This is Shop!</h1>;
+}
 // Routes allows this application to register these root level components
 // that will then in turn render a specific component when it matches this specific route that we are looking for.
 const App = () => {
   return (
+    // index attribute tell this route is that when you match just this slash,
+    // then this should be the home component
     <Routes>
-      <Route path='/' element={<Home /> } />
+      <Route path='/' element={<Navigation />} >
+      
+        <Route index={true} element={<Home />} /> 
+        <Route path='shop' element={<Shop />}/>  
+      </Route>
+
     </Routes>  
     );
 }
