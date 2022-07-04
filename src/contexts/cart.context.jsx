ab3e6634ac9a1ea2074d930243482e23e0690a56
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-07-04 14:01:27 -0400

    Added reducer hooks

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,5 @@
-import { createContext,useState,useEffect } from "react";
+import { createContext, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.util";
 
 const addCartItem = (cartItems, productToAdd) => {
     // find if cart item contains productToAdd
@@ -47,36 +48,88 @@ export const CartContext = createContext({
     cartTotalPrice:0,
 });
 
+const INITIAL_STATE = {
+    isCartOpen: false,
+    cartItems: [],
+    cartCount: 0,
+    cartTotalPrice:0
+}
+
+const CART_ACTION_TYPE = {
+    SET_CART_ITEMS: 'SET_CART_ITEMS',
+    SET_IS_CART_OPEN:'SET_IS_CART_OPEN'
+}
+
+const cartReducer = (state, action) => {
+    const { type, payload } = action;
+    // Note : Reducer should not handle any business logic, meaning that inside of a reducer
+    // payload should already be whatever we need to update.
+    // reducer should care about is really just updating the state, should not worry about anything regarding how to update that state.
+    switch (type) {
+        case CART_ACTION_TYPE.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload
+            }
+        case CART_ACTION_TYPE.SET_IS_CART_OPEN:
+            return {
+                ...state,
+                isCartOpen:payload
+            }
+     
+        default:
+            throw new Error(`Unhandled type ${type} in cartReducer`);
+    }
+}
+// Reducer allow us to conjugate some of logic into one separate function and primarily that the good time to use reducer is when one update 
+// needs to modify multiple readable values inside of state
 export const CartProvider = ({ children }) => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const [cartItems, setCartItem] = useState([]);
-    const [cartCount, setCartCount] = useState(0);
-    const [cartTotalPrice, setCartTotalPrice] = useState(0);
+    const [{ cartItems, isCartOpen, cartCount, cartTotalPrice }, dispatch] = useReducer(cartReducer, INITIAL_STATE);
 
-    useEffect(() => { 
-        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
-        setCartCount(newCartCount);
-    }, [cartItems]);
+    
+    const updateCartItemReducer = (newCartItems) => {
+        /**
+         * generate newCartTotal
+         * generate newCartCount
+         * 
+         * dispatch new action with payload = {
+         *  newCartitems,
+         *  newCartTotal,
+         *  newCartCount
+         * }
+         */
+        const newCartCount = newCartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
 
-    // Although it works as cartCount, however, create a new useEffect
-    // This is the best practice when using effect make sure it governs one singular responsibility
-    useEffect(() => {
-        const newCartTotalPrice = cartItems.reduce((totalPrice, cartItem) => totalPrice + cartItem.quantity * cartItem.price, 0);
-        setCartTotalPrice(newCartTotalPrice);
-    },[cartItems]);
+        const newCartTotalPrice = newCartItems.reduce((totalPrice, cartItem) => totalPrice + cartItem.quantity * cartItem.price, 0);
+        dispatch(
+            createAction(CART_ACTION_TYPE.SET_CART_ITEMS, {
+                cartItems: newCartItems,
+                cartTotalPrice: newCartTotalPrice,
+                cartCount: newCartCount
+            })
+        );
+    }
+
     // addItemToCart triggers whenever a user clicks on button
     const addItemToCart = (productToAdd) => { 
-        setCartItem(addCartItem(cartItems, productToAdd));
+        const newCartItems = addCartItem(cartItems, productToAdd);
+        updateCartItemReducer(newCartItems);
     }
 
     const removeItemFromCart = (productToRemove) => { 
-        setCartItem(removeCartItem(cartItems, productToRemove));    
+        const newCartItems = removeCartItem(cartItems, productToRemove);    
+        updateCartItemReducer(newCartItems);
     }
 
     const clearItemFromCart = (productToClear) => {
-        setCartItem(clearCartItem(cartItems, productToClear));    
+        const newCartItems = clearCartItem(cartItems, productToClear);    
+        updateCartItemReducer(newCartItems);
     }
 
-    const value = { isCartOpen,setIsCartOpen ,addItemToCart,cartItems,cartCount,removeItemFromCart,cartTotalPrice,clearItemFromCart};
+    const setIsCartOpen = (newCartStatus) => {
+        dispatch(createAction(CART_ACTION_TYPE.SET_IS_CART_OPEN, newCartStatus));
+    }
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemFromCart, cartTotalPrice, clearItemFromCart };
     return (<CartContext.Provider value={value}>{children}</CartContext.Provider>);
 }
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,19 +1,56 @@
 import { useEffect } from "react";
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 import { onAuthStateChangedListener ,createUserDocumentFromAuth} from "../utils/firebase/firebase.utils";
 
+// Context is still is context, because the context is what values that we expose.
+// The main differentce now is that how we are storing current user (use useReducer instead of useState)
 // as the actual value want to access
 export const UserContext = createContext({
     currentUser: null,
     setCurrentUser:() => null,
 });
 
+export const USER_ACTION_TYPE = {
+    SET_CURRENT_USER:'SET_CURRENT_USER'
+}
+const userReducer = (state, action) => {
+    console.log('dispatched');
+    console.log(action);
+    // payload is going to store the value that is important for this reducer to know what to update this state value with.
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPE.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser:payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser:null
+}
+
 // every context that get built for us, 
 // there is a dot provider and the provider is the component that will 
 // wrap around any other components that need access to the value inside.
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    // userReducer (someReducer,initalValue) always return 2 arguments
+    // first one is the state object
+    // second one is dispatch function (whenver we call dispatch function, we pass it an action object)
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    console.log(currentUser);
+
+    // const { currentUser } = state;
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPE.SET_CURRENT_USER, payload: user });
+    }
+
     const value = { currentUser, setCurrentUser };
+
     useEffect(() => {
         // whenver unmount it will unsubscribe the listener otherwise it will rasie memeory leak issue
         const unsubscribe = onAuthStateChangedListener((user) => { 
@@ -28,3 +65,16 @@ export const UserProvider = ({ children }) => {
     // call setter and get the value anywhere inside of the component tree that is nested within actual provider value.
     return <UserContext.Provider value={ value }>{ children }</UserContext.Provider>
 }
+
+// Reducer is just function that always return back an new object (state in the reducer - The object reflects whatever the actual current value is going to be).
+// The reason why we always return a new object is because that is how React determines that something has changed.
+// As a result if reducer change, then the object must be different.
+// The reducers is that change the object that we get back and the properties and value inside them based on the action.
+
+/*
+    const userReducer = (state,action) = >{
+         return {
+         currentUser:
+         }
+    }
+ */
